e5c70e9705334899fa9f0196324706bf3dcdb2c3
Moth <moth.feedback@gmail.com>
2024-10-13 18:07:39 +0300

    upd

--- main_1.22.5_en/stop-and-delete-container.sh
+++ main_1.22.5_en/stop-and-delete-container.sh
@@ -1,10 +1,5 @@
 #!/bin/env sh
 
 docker stop knuckle-go-container
-docker rm knuckle-go-container --force --if-exists
-if [[ "$(docker images -q my-custom-image 2> /dev/null)" != "" ]]; then
-    docker rmi my-custom-image --force
-else
-    echo "Образ не существует"
-fi
-
+docker rm knuckle-go-container --force
+docker rmi knuckle-go --force
